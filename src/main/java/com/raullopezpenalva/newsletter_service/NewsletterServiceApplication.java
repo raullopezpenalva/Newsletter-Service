69fa73300b1c6c4662685db08fffaa5a3d882d8d
Raul Lopez Penalva <raullopezpenalva@icloud.com>
2025-09-16 09:50:05 +0200

    Add files via upload

---
+++ src/main/java/com/raullopezpenalva/newsletter_service/NewsletterServiceApplication.java
@@ -0,0 +1,13 @@
+package com.raullopezpenalva.newsletter_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsletterServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsletterServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raullopezpenalva/newsletter_service/NewsletterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raullopezpenalva.newsletter_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsletterServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
